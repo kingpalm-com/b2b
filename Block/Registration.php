7d5501b91397bbc0d58bbc74723c071af7e4fb89
Dmitry Fedyuk <dfediuk@gmail.com>
2019-05-30 15:20:42 +0600

    no message

--- Block/Registration.php
+++ Block/Registration.php
@@ -1,9 +1,11 @@
 <?php
 namespace KingPalm\B2B\Block;
+use Df\Framework\Form\Element as E;
 use Df\Framework\Form\Element\Checkbox;
 use Df\Framework\Form\Element\Text;
 use KingPalm\B2B\Renderer;
 use Magento\Framework\Data\Form;
+use Magento\Framework\Data\Form\Element\AbstractElement as AE;
 use Magento\Framework\View\Element\Template as _P;
 /**
  * 2019-05-30
@@ -16,17 +18,35 @@ class Registration extends _P {
 	 * @param $label
 	 * @return string
 	 */
-	function text($id, $label) {
-		$e = df_new_omd(Text::class, [
-			'html_id' => $id, 'label' => $label, 'name' => "kingpalm_business_$id"
-		]); /** @var Text $e */
+	function cb($id, $label) {return $this->e(Checkbox::class, $id, $label);}
+
+	/**
+	 * 2019-05-30
+	 * @param $id
+	 * @param $label
+	 * @return string
+	 */
+	function text($id, $label) {return $this->e(Text::class, $id, $label);}
+
+	/**
+	 * 2019-05-30
+	 * @used-by cb()
+	 * @used-by text()
+	 * @param string $c
+	 * @param $id
+	 * @param $label
+	 * @return string
+	 */
+	private function e($c, $id, $label) {
+		/** @var E|AE $e */
+		$e = df_new_omd($c, ['html_id' => $id, 'label' => $label, 'name' => "kingpalm_business_$id"]);
 		$e->setForm($this->form());
 		return $this->r()->render($e);
 	}
 
 	/**
 	 * 2019-05-30
-	 * @used-by text()
+	 * @used-by e()
 	 * @return Form
 	 */
 	private function form() {return dfc($this, function() {return
@@ -35,7 +55,7 @@ class Registration extends _P {
 
 	/**
 	 * 2019-05-30
-	 * @used-by text()
+	 * @used-by e()
 	 * @return Renderer
 	 */
 	private function r() {return dfc($this, function() {return new Renderer;});}
