95ad4f562c9ddfc9a83c88275846cc6bb5b6e130
Dmitry Fedyuk <dfediuk@gmail.com>
2019-06-05 18:13:42 +0600

    no message

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -1,5 +1,6 @@
 <?php
 namespace KingPalm\B2B\Setup;
+use Df\Framework\DB\ColumnType as T;
 use KingPalm\B2B\Schema as S;
 // 2019-06-04
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
@@ -12,9 +13,8 @@ class UpgradeSchema extends \Df\Framework\Upgrade\Schema {
 	 */
 	final protected function _process() {
 		if ($this->isInitial()) {
-			// 2019-06-05 @todo Add comments to columns.
-			df_dbc_c(S::name());
-			df_dbc_c(S::enable());
+			df_dbc_c(S::name(), S::name(true));
+			df_dbc_c(S::enable(), T::bool(S::enable(true)));
 		}
 	}
 }
