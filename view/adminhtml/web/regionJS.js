d42190136d1300c63ad782ce3dbfbdff70aceb7d
Dmitry Fedyuk <dfediuk@gmail.com>
2019-06-12 13:02:24 +0600

    no message

--- view/adminhtml/web/regionJS.js
+++ view/adminhtml/web/regionJS.js
@@ -1,25 +1,26 @@
 // 2019-06-12
-define(['jquery', 'KingPalm_B2B/regionUpdater'], function($) {return function(d) {
-	var wait = function() {
-		if (!$(d.country).length || !$(d.region).length || !$(d.regionId).length) {
-			setTimeout(wait, 1000);
-		}
-		else setTimeout(function() {
-			/** @type {HTMLSelectElement} */ var $country = $(d.country);
-			/** @type {HTMLInputElement} */ var $region = $(d.region);
-			/** @type {HTMLInputElement} */ var $regionId = $(d.regionId);
-			/** @type {HTMLInputElement} */ var $postcode = $(d.postcode);
-			$country.regionUpdater({
-				countriesWithOptionalZip: d.countriesWithOptionalZip
-				,defaultRegion: ''
-				,form: null
-				,optionalRegionAllowed: true
-				,postcodeId: '#' + $postcode.attr('id')
-				,regionInputId: '#' + $region.attr('id')
-				,regionJson: d.json
-				,regionListId: '#' + $regionId.attr('id')
-			});
-		}, 500);
+define(['df-lodash', 'jquery', 'KingPalm_B2B/regionUpdater'], function(_, $) {return function(d) {
+	/**
+	 * 2019-06-12
+ 	 * @param {String[]} ee
+	 * @param {Function} cb
+	 */
+	var wait = function(ee, cb) {
+		!_.find(ee, function(e) {return !$(e).length})
+			? cb()
+			: setTimeout(function() {wait(ee, cb);}, 1000)
+		;
 	};
-	wait();
+	wait([d.country, d.region, d.regionId], function() {
+		$(d.country).regionUpdater({
+			countriesWithOptionalZip: d.countriesWithOptionalZip
+			,defaultRegion: ''
+			,form: null
+			,optionalRegionAllowed: true
+			,postcodeId: '#' + $(d.postcode).attr('id')
+			,regionInputId: '#' + $(d.region).attr('id')
+			,regionJson: d.json
+			,regionListId: '#' + $(d.regionId).attr('id')
+		});
+	});
 }});
